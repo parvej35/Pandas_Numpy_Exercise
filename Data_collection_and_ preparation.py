21e21b7081b94c1da0a207bae88a2d4efad01865
Parvej Ahmed Chowdhury <parvej35@gmail.com>
2021-11-30 00:35:22 +0100

    Initial commit

--- Data_collection_and_ preparation.py
+++ Data_collection_and_ preparation.py
@@ -1,19 +1,21 @@
 import pandas as pd
 import numpy as np
 
-dataset = pd.read_csv("data/heart_failure_clinical_records_dataset.csv")
+# dataset = pd.read_csv("data/heart_failure_clinical_records_dataset.csv")
+dataset = pd.read_csv("data/fitbit_export_202111_parvej.csv")
 
+# dataset = dataset.drop(dataset.index[0])
 # Show all column names
-# print(dataset.columns)
+print(dataset.columns)
 
 # Show top 5 rows
-# print(dataset.head(5))
+print(dataset.head())
 
 # Show bottom 5 rows
 # print(dataset.tail(5))
 
 # Show only selected columns of the dataset
-temp_dataset = dataset[['age', 'diabetes', 'high_blood_pressure', 'platelets', 'time']]
+# temp_dataset = dataset[['age', 'diabetes', 'high_blood_pressure', 'platelets', 'time']]
 # print(temp_dataset.head(5))
 
 # Show total number of data rows X columns
@@ -23,19 +25,19 @@ print(dataset.shape)
 # print(dataset.describe())
 
 # Drop the garbage data (index 299)
-dataset = dataset.drop(dataset.index[299])
+# dataset = dataset.drop(dataset.index[299])
 # dataset.drop(dataset.index[299], inplace=True)
 # print(dataset.tail())
 
 # Convert ROW-> COLUMN & COLUMN-> ROW
-# print(dataset)
-dataset = dataset.transpose()
+print(dataset)
+# dataset = dataset.transpose()
 # print(dataset)
 
 # Take a sample of randomly chosen 5 columns, because 300 takes too much memory
-temp_dataset = dataset.sample(5, axis="columns")
-print(temp_dataset)
+# temp_dataset = dataset.sample(5, axis="columns")
+# print(temp_dataset)
 
-print(temp_dataset.info())
+# dataset.info()
 
 
